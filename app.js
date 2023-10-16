31ab42e5ed5467d121ad7190fb7d373819f37bcc
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2023-10-16 21:30:27 +0800

    IHIS-198
    JWT for image server

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { body, validationResult } = require("express-validator");
+var { expressjwt: jwt } = require("express-jwt");
+const { JWT_SECRET } = require("./constants/JwtSecret");
 
 const app = express();
 const PORT = PORT_NUM;
@@ -47,6 +49,38 @@ app.use(
   })
 );
 
+//checking for JWT
+app.use((req, res, next) => {
+  let authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    let token = authHeader.split(" ")[1];
+    console.log("Received Token:", token);
+  } else {
+    console.log("No Token Received");
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    let authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      let token = authHeader.split(" ")[1];
+      console.log("Attempting to verify token:", token);
+    }
+    try {
+      jwt({
+        secret: Buffer.from(JWT_SECRET, "base64"),
+        algorithms: ["HS256"],
+      })(req, res, next);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    next();
+  }
+});
+
 // Serve static images from different folders
 app.use(
   "/images/id",
@@ -70,6 +104,13 @@ app.post("/upload/:type", upload.single("image"), (req, res) => {
   }
 });
 
+//GET endpoint for frontend to receive image
+// app.get('/path-to-image', (req, res) => {
+//   const imgBuffer = ...; // Load your image into a buffer
+//   res.setHeader('Content-Type', 'image/png');
+//   res.send(imgBuffer);
+// });
+
 // Validate upload middleware using express validator
 // const validateUpload = [
 //     // Validate 'type' parameter
@@ -84,6 +125,26 @@ app.post("/upload/:type", upload.single("image"), (req, res) => {
 //       return true;
 //     }),
 //   ];
+
+// Throws error if invalid JWT Token
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    console.error(err.message);
+    if (err.message === "jwt expired") {
+      return res.status(401).send("JWT Token has expired");
+    } else {
+      return res.status(401).send("Invalid token");
+    }
+  } else {
+    // Pass the error to any subsequent error handlers.
+    next(err);
+  }
+});
+
+// Catch-all for any request that doesn't match the above routes.
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
---
+++ constants/JwtSecret.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "67676b613166584b3161706a794f4b6369414c4733773d3d";
+
+module.exports = { JWT_SECRET };
